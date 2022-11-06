1d52bedc3b467eddaf8c0965b6025c47d50a7466
Emmanuel289 <emmirald@gmail.com>
2022-11-06 08:07:48 -0700

    Added a helper method in the functional test that checks for saved items in the to-do list

--- functional_tests.py
+++ functional_tests.py
@@ -12,6 +12,11 @@ class NewVisitorTest(unittest.TestCase):
         # Edith is satisfied, so she goes back to sleep
         self.browser.quit()
 
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
+
     def test_can_start_a_list_and_retrieve_it_later(self):
         # Edith has heard about a cool new online to-do app. She goes to check          # out its homepage
         self.browser.get('http://localhost:8000')
@@ -22,6 +27,7 @@ class NewVisitorTest(unittest.TestCase):
         self.assertIn('To-Do', header_text)
 
         # She is invited to enter a to-do item straight away
+        input_box = self.browser.find_element_by_id("id_new_item")
         self.assertEqual(
                 input_box.get_attribute('placeholder'),
                 'Enter a to-do item'
@@ -34,25 +40,19 @@ class NewVisitorTest(unittest.TestCase):
         # "1: Buy peacock feathers" as an item in a to-do list
         input_box.send_keys(Keys.ENTER)
         time.sleep(1)
-        
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn("1: Buy peacock feathers", [row.text for row in rows])
+        self.check_for_row_in_list_table("1:Buy peacock feathers")
+
         # There is still a text box inviting her to add another item. She
         # enters "Use peacock feathers to make a fly" (Edith is very methodical)
-        input_box.find_element_by_id('id_new_item')
+        input_box = self.browser.find_element_by_id('id_new_item')
         input_box.send_keys('Use peacock feathers to make a fly')
         input_box.send_keys(Keys.ENTER)
-        time.sleep(5)
+        time.sleep(1)
 
         # The page updates again, and now shows both items on her list
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
-        self.assertIn('2: Use peacock feathers to make a fly',
-                [row.text for row in rows]
-        )
-
+        self.check_for_row_in_list_table("1: Buy peacock feathers")
+        self.check_for_row_in_list_table("2: Use peacock feathers to make a fly")
+        
         # Edith wonders whether the site will remember her list. Then she sees
         # that the site has generated a unique url for her -- there is some
         # explanatory text to that effect.
