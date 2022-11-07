5a53438265be1b6af5740a3353b3107bfdaa2028
Emmanuel289 <emmirald@gmail.com>
2022-11-07 16:37:12 -0700

    new URL + view for adding to existing lists. FT broken

--- superlists/urls.py
+++ superlists/urls.py
@@ -18,5 +18,6 @@ from lists import views
 urlpatterns = [
     path('', views.home_page, name='home'),
     path('lists/new', views.new_list, name='new list'),
-    path('lists/the-only-list-in-the-world/', views.view_list, name='view_list'),
+    path('lists/<int:list_id>/', views.view_list, name='view_list'),
+    path('lists/<int:list_id>/add_item/', views.add_item, name='add item')
 ]
