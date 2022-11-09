21d6484f7a314a90df5141ffad3bd613cac9c0d2
Emmanuel289 <emmirald@gmail.com>
2022-11-08 21:05:49 -0700

    first steps of FT for layout and styling

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -27,7 +27,8 @@ class NewVisitorTest(LiveServerTestCase):
                 time.sleep(0.5)
 
     def test_can_start_a_list_for_one_user(self):
-        # Edith has heard about a cool new online to-do app. She goes to check          # out its homepage
+
+        # Edith has heard about a cool new online to-do app. She goes to check out its homepage
         self.browser.get(self.live_server_url)
 
         # She notices the page title and header mention to-do lists
@@ -109,6 +110,31 @@ class NewVisitorTest(LiveServerTestCase):
         page_text = self.browser.find_element_by_tag_name('body').text
         self.assertNotIn('Buy peacock feathers', page_text)
         self.assertIn('Buy milk', page_text)
+
+
+    def test_layout_and_styling(self):
+        # Edith goes to the home page
+        self.browser.get(self.live_server_url)
+        self.browser.set_window_size(1024, 786)
+
+        # She notices the input box is nicely centered
+        input_box = self.browser.find_element_by_id('id_new_item')
+        self.assertAlmostEqual(
+            input_box.location['x'] + input_box.size['width'] / 2,
+            512,
+            delta=10
+        )
+
+        # She starts a new list and sees the input is nicely centered there too
+        input_box.send_keys('testing')
+        input_box.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: testing')
+        input_box = self.browser.find_element_by_id('id_new_item')
+        self.assertAlmostEqual(
+            input_box.location['x'] + input_box.size['width'] / 2,
+            512,
+            delta=10
+        )
 
         # Satisfied, they both go back to sleep
 
