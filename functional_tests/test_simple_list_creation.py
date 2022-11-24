3a07499c49a853d319b83d81f41487c806c1ca36
Emmanuel289 <emmirald@gmail.com>
2022-11-24 16:01:57 -0700

    Ajust new list view to do model validation

--- functional_tests/test_simple_list_creation.py
+++ functional_tests/test_simple_list_creation.py
@@ -1,4 +1,3 @@
-from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium import webdriver 
 from selenium.webdriver.common.keys import Keys
 from .base import FunctionalTest
