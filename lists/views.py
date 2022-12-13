f1a1aa44125556f1d4a4c23a2fecd048a463eeed
Emmanuel289 <emmirald@gmail.com>
2022-12-13 04:44:44 -0700

    Removed trailing blank lines

--- lists/views.py
+++ lists/views.py
@@ -31,6 +31,3 @@ def new_list(request):
         error = "You can't have an empty list item"
         return render(request, 'home.html', {"error": error})
     return redirect(f'/lists/{list_.id}/')
-
-
-    
