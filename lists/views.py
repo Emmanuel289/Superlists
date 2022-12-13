7a809ba68e810fefa85cbef436a4d2bee9008764
Emmanuel289 <emmirald@gmail.com>
2022-12-13 05:36:17 -0700

    Included test for deleting invalid list items

--- lists/views.py
+++ lists/views.py
@@ -27,6 +27,7 @@ def new_list(request):
         item.full_clean()
         item.save()
     except ValidationError:
+        item.delete()
         list_.delete()
         error = "You can't have an empty list item"
         return render(request, 'home.html', {"error": error})
